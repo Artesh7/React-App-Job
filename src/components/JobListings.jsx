d78d0aba39c0269f33fc2e41bee894f644ea87b3
Artesh7 <333075@via.dk>
2024-11-07 15:46:24 +0100

    Creating proxy to the url in vite.config

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -7,8 +7,9 @@ const JobListings = ({ isHome = false }) => {
   const [loading, setLoading] = useState(true);
   useEffect(() => {
     const fetchJobs = async () => {
+      const apiUrl = isHome ? "/api/jobs?_limit=3" : "/api/jobs";
       try {
-        const res = await fetch("http://localhost:8000/jobs");
+        const res = await fetch(apiUrl);
         const data = await res.json();
         setJobs(data);
       } catch (error) {
